6ac8b7aa55b061e31854cbd49d561daa2f969bb5
Matt <mattdirlik@gmail.com>
2023-02-04 18:30:16 +0000

    Use HomeScreen in MainActivity

--- app/src/main/kotlin/com/example/cartalkuk/MainActivity.kt
+++ app/src/main/kotlin/com/example/cartalkuk/MainActivity.kt
@@ -3,16 +3,11 @@ package com.example.cartalkuk
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
+import com.example.cartalkuk.ui.HomeScreen
 import com.example.cartalkuk.ui.theme.AntisocialmediaTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,34 +15,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AntisocialmediaTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column {
-                        AppTitle()
-                    }
+                    HomeScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun AppTitle(modifier: Modifier = Modifier) {
-    Text(
-        modifier = modifier,
-        text = "Welcome to the Anti-Social Media app",
-        textAlign = TextAlign.Center,
-        fontSize = 24.sp
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AntisocialmediaTheme {
-        AppTitle()
     }
 }
