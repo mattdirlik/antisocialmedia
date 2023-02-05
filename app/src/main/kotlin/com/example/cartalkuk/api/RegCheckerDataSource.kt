3e2e8a8c6609bdc6c054b3bf0f6912a048f020c0
Matt <mattdirlik@gmail.com>
2023-02-05 19:09:21 +0000

    Create DataSource

---
+++ app/src/main/kotlin/com/example/cartalkuk/api/RegCheckerDataSource.kt
@@ -0,0 +1,13 @@
+package com.example.cartalkuk.api
+
+import com.example.cartalkuk.data.model.VehicleEnquiryRequestModel
+import com.example.cartalkuk.data.model.VehicleEnquiryResponseModel
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface RegCheckerDataSource {
+    @POST("vehicle-enquiry/v1/vehicles")
+    suspend fun getVehicleDetails(
+        @Body request: VehicleEnquiryRequestModel
+    ): VehicleEnquiryResponseModel
+}
