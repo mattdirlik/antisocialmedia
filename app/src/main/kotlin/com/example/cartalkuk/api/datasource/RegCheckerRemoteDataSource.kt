436135374b2aec11ceb3dee12262320be9d2cc61
Matt <mattdirlik@gmail.com>
2023-02-07 19:21:19 +0000

    remove retrofit annotations from RemoteDataSource

--- app/src/main/kotlin/com/example/cartalkuk/api/datasource/RegCheckerRemoteDataSource.kt
+++ app/src/main/kotlin/com/example/cartalkuk/api/datasource/RegCheckerRemoteDataSource.kt
@@ -3,16 +3,13 @@ package com.example.cartalkuk.api.datasource
 import com.example.cartalkuk.api.RegCheckerRetrofitObject
 import com.example.cartalkuk.data.model.VehicleEnquiryRequestModel
 import com.example.cartalkuk.data.model.VehicleEnquiryResponseModel
-import retrofit2.http.Body
-import retrofit2.http.POST
 
 class RegCheckerRemoteDataSource(
     private val service: RegCheckerDataSource = RegCheckerRetrofitObject.retrofit
 ) : RegCheckerDataSource {
 
-    @POST("vehicle-enquiry/v1/vehicles")
     override suspend fun getVehicleDetails(
-        @Body request: VehicleEnquiryRequestModel
+        request: VehicleEnquiryRequestModel
     ): VehicleEnquiryResponseModel {
         return service.getVehicleDetails(request)
     }
