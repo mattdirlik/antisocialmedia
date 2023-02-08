9e85361b8423a965427e52775444220b4065787f
Matt <mattdirlik@gmail.com>
2023-02-08 22:18:50 +0000

    change theme name

--- app/src/main/kotlin/com/example/cartalkuk/MainActivity.kt
+++ app/src/main/kotlin/com/example/cartalkuk/MainActivity.kt
@@ -6,13 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import com.example.cartalkuk.ui.home.HomeScreen
 import com.example.cartalkuk.ui.appwide.CarTalkTopBar
-import com.example.cartalkuk.ui.theme.AntisocialmediaTheme
+import com.example.cartalkuk.ui.theme.RegCheckerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AntisocialmediaTheme {
+            RegCheckerTheme {
                 Column {
                     CarTalkTopBar()
                     HomeScreen()
