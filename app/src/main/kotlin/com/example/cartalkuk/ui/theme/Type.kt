a6f23e0fe55e469f726db277c9eb93b07a1f42a4
Matt <mattdirlik@gmail.com>
2023-02-08 22:18:13 +0000

    add Maven Pro as default font

--- app/src/main/kotlin/com/example/cartalkuk/ui/theme/Type.kt
+++ app/src/main/kotlin/com/example/cartalkuk/ui/theme/Type.kt
@@ -2,33 +2,46 @@ package com.example.cartalkuk.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.cartalkuk.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+val MavenStandard = FontFamily(
+    Font(
+        resId = R.font.mavenpro
     )
-    /* Other default text styles to override
+)
+
+val Typography = Typography(
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
+        fontFamily = MavenStandard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 24.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
+    titleMedium = TextStyle(
+        fontFamily = MavenStandard,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        letterSpacing = 0.15.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = MavenStandard,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+        lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    */
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = MavenStandard,
+        fontSize = 16.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = MavenStandard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        letterSpacing = 1.25.sp
+    ),
 )
