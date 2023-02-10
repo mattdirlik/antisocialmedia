26e6c43d83a87f190aee31d40d35077afcba7094
Matt <mattdirlik@gmail.com>
2023-02-10 23:32:30 +0000

    add drawer sheet for settings menu

--- app/src/main/kotlin/com/example/cartalkuk/MainActivity.kt
+++ app/src/main/kotlin/com/example/cartalkuk/MainActivity.kt
@@ -4,9 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import com.example.cartalkuk.ui.home.HomeScreen
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Surface
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.LayoutDirection
+import com.example.cartalkuk.settings.ColourThemeOptions
 import com.example.cartalkuk.ui.appwide.CarTalkTopBar
+import com.example.cartalkuk.ui.home.HomeScreen
 import com.example.cartalkuk.ui.theme.RegCheckerTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,8 +29,42 @@ class MainActivity : ComponentActivity() {
         setContent {
             RegCheckerTheme {
                 Column {
-                    CarTalkTopBar()
-                    HomeScreen()
+                    val drawerState = rememberDrawerState(DrawerValue.Closed)
+                    val scope = rememberCoroutineScope()
+
+                    CarTalkTopBar(
+                        onSettingsClicked = {
+                            scope.launch {
+                                if (drawerState.isOpen) {
+                                    drawerState.close()
+                                } else {
+                                    drawerState.open()
+                                }
+                            }
+                        }
+                    )
+
+                    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+                        ModalNavigationDrawer(
+                            drawerState = drawerState,
+                            gesturesEnabled = drawerState.isOpen,
+                            drawerContent = {
+                                Surface(
+                                    modifier = Modifier
+                                        .fillMaxHeight()
+                                        .width(IntrinsicSize.Max),
+                                    color = MaterialTheme.colorScheme.background
+                                ) {
+                                    ColourThemeOptions()
+                                }
+                            },
+                            content = {
+                                CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
+                                    HomeScreen()
+                                }
+                            }
+                        )
+                    }
                 }
             }
         }
