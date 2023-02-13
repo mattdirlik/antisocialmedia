4422e72494284cab3fc96f3da8122584a56097fd
Matt <mattdirlik@gmail.com>
2023-02-13 00:02:17 +0000

    change status bar colour

--- app/src/main/kotlin/com/example/cartalkuk/ui/theme/Theme.kt
+++ app/src/main/kotlin/com/example/cartalkuk/ui/theme/Theme.kt
@@ -61,8 +61,9 @@ fun RegCheckerTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars =
+                colorScheme == LightColorScheme
         }
     }
 
